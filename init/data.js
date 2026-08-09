f0f120139e06bb9049ec98182982500315bfbc2e
Piyush Kumar <piyushkumarg292007@gmail.com>
2026-08-09 19:41:44 +0530

    Initial Phase of this Project Level One

---
+++ init/data.js
@@ -0,0 +1,60 @@
+const sampleListings = [
+  {
+    title: "Paneer Butter Masala",
+    description: "Rich and creamy dish made with paneer, spices, onions, tomatoes, cashews, and butter.",
+    image: "istockphoto.com/photo/paneer-butter-masala-also-known-as-panir-makhani-or-makhanwala-served-in-a-ceramic-gm1024585168-274910253?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcreamy-butter-paneer-masala&utm_term=creamy+butter+paneer+masala%3A%3Asearch-affiliate%3Aexperiment%3A9a0dd143-698b-4b85-938a-0941f905dc75", // Image: Creamy Paneer Butter Masala / Indian Curry
+    price: 280,
+    category: "Main Course",
+    isVeg: true
+  },
+  {
+    title: "Chicken Biryani",
+    description: "A aromatic and flavorful rice dish made with basmati rice, tender chicken pieces, and fresh spices.",
+    image: "https://www.istockphoto.com/photo/chicken-biryani-11-gm501150349-43430656?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fchicken-biryani&utm_term=chicken+biryani%3A%3Asearch-affiliate%3Aexperiment%3A87a2ad7f-a813-4c2e-b3dd-f98b68d05c62", // Image: Chicken Biryani with Raita
+    price: 3500,
+    category: "Main Course",
+    isVeg: false
+  },
+  {
+    title: "Crispy Masala Dosa",
+    description: "South Indian thin crepe made from fermented batter, filled with spiced potato mash, served with chutney and sambar.",
+    image: "https://unsplash.com/photos/a-plate-of-food-5oF7d_hPJG4", // Image: South Indian Masala Dosa with Chutney
+    price: 150,
+    category: "Breakfast / Snacks",
+    isVeg: true
+  },
+  {
+    title: "Classic Margherita Pizza",
+    description: "Fresh pizza topped with rich tomato sauce, mozzarella cheese, and fresh basil leaves.",
+    image: "https://unsplash.com/photos/a-pizza-sitting-on-top-of-a-wooden-cutting-board-dFE0FNVd4k0", // Image: Margherita Pizza
+    price: 399,
+    category: "Italian / Fast Food",
+    isVeg: true
+  },
+  {
+    title: "Juicy Chicken Burger",
+    description: "Crispy and juicy chicken patty loaded with lettuce, cheese slice, tomato, and special burger sauce.",
+    image: "unsplash.com/photos/a-chicken-sandwich-on-a-plate-next-to-a-basket-of-fries-8JklXjB2X3w", // Image: Chicken Burger with Fries
+    price: 220,
+    category: "Fast Food",
+    isVeg: false
+  },
+  {
+    title: "Gulab Jamun (2 Pcs)",
+    description: "Soft, melt-in-the-mouth fried dumplings made of milk solids, soaked in warm cardamom sugar syrup.",
+    image: "https://www.istockphoto.com/photo/gulab-jamun-gm1363048391-434757954?utm_source=unsplash&utm_medium=affiliate&utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fgulab-jamun&utm_term=gulab+jamun%3A%3Asearch-affiliate%3Aexperiment%3Acbf21600-3cc7-4c13-b8d9-11e36963dd8c", // Image: Gulab Jamun Dessert
+    price: 90,
+    category: "Desserts",
+    isVeg: true
+  },
+  {
+    title: "Cold Coffee with Ice Cream",
+    description: "Rich and frothy blended cold coffee topped with a scoop of vanilla ice cream and chocolate syrup.",
+    image: "https://unsplash.com/photos/ice-cream-on-clear-glass-cup-FRChhrMEbnA", // Image: Cold Coffee Glass
+    price: 140,
+    category: "Beverages",
+    isVeg: true
+  }
+];
+
+module.exports = { data: sampleListings };
---
+++ models/listing.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type: String,
+        default:"https://www.google.com/search?sca_esv=fd80582d8fba4559&rlz=1C1CHBF_enIN820IN820&sxsrf=APpeQnu_mVV6sZVCnSgVWxZ891QLF2kxrg:1786101320047&q=sorry+something+oops+sorry+try+again+image&uds=AJ5uw1_a2D0D09lxm8gpKKOTUn4rUa0D3GoYUcxe7VXlYKBH8_EeSkAsx5Vbm0HH8WqMPweOrPWYE_lEjFA1DJTwL4xPr9-FEljbHg9RnrgPbKHvAiavqX9W9R2VoEtShaHKuGsYTfeFLGNDPrHMe4HLG15fMChKT8UC883MBAvrb0LqtP3fyxc&udm=2&sa=X&ved=2ahUKEwiQgdv1sY6WAxUHWHADHRxWIY8QxKsJKAd6BQj1BRAB&ictx=0&biw=1366&bih=641&dpr=1#sv=CAMSURoyKhBlLVdxMGZTTXdaaGRjTktNMg5XcTBmU013WmhkY05LTToOX1VNQWN3VDZyREZVLU0gBCoXCgFzEhBlLVdxMGZTTXdaaGRjTktNGAEwARgHIObpq5wGSggQARgBIAEoAQ",
+        required: true,
+    },
+    description: String,
+    image:{
+        type: String,
+        set: (v) => v === "" ? "https://www.google.com/search?sca_esv=fd80582d8fba4559&rlz=1C1CHBF_enIN820IN820&sxsrf=APpeQnu_mVV6sZVCnSgVWxZ891QLF2kxrg:1786101320047&q=sorry+something+oops+sorry+try+again+image&uds=AJ5uw1_a2D0D09lxm8gpKKOTUn4rUa0D3GoYUcxe7VXlYKBH8_EeSkAsx5Vbm0HH8WqMPweOrPWYE_lEjFA1DJTwL4xPr9-FEljbHg9RnrgPbKHvAiavqX9W9R2VoEtShaHKuGsYTfeFLGNDPrHMe4HLG15fMChKT8UC883MBAvrb0LqtP3fyxc&udm=2&sa=X&ved=2ahUKEwiQgdv1sY6WAxUHWHADHRxWIY8QxKsJKAd6BQj1BRAB&ictx=0&biw=1366&bih=641&dpr=1#sv=CAMSURoyKhBlLVdxMGZTTXdaaGRjTktNMg5XcTBmU013WmhkY05LTToOX1VNQWN3VDZyREZVLU0gBCoXCgFzEhBlLVdxMGZTTXdaaGRjTktNGAEwARgHIObpq5wGSggQARgBIAEoAQ" : v,
+    },
+    price: Number,
+    category: {
+        type: String,
+        
+    },
+    isVeg:{
+        type: Boolean,
+        default: true,
+    },
+    restaurantName: String,
+    city: String,
+    preparationTime: Number,
+    spiciness: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
