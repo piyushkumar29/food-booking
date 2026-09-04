e9158030e7ce31e556d03c093cda1f43992c2974
Piyush Kumar <piyushkumarg292007@gmail.com>
2026-09-04 22:42:28 +0530

    review and authentication work is going on

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -26,68 +34,63 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
+
+
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-// Index route
-app.get("/listings", async (req, res) => {
-     const allListings = await Listing.find({});
-     res.render("listings/index.ejs", {allListings});
-    
-});
-// New route
-app.get("/listings/new", (req, res) =>{
-    res.render("listings/new.ejs");
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error= req.flash("error");
+    next();
 });
 
-//show route
-app.get("/listings/:id", async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student"
+    });
+
+    let registeredUser = await User.register(fakeUser, "helloworld");
+    res.send(registeredUser);
 });
 
-// create Route
-app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
+
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
+
+app.use((req, res, next) => {
+    next(new ExpressError(404, "Page not found!"));
 });
 
-// Edit route
-app.get("/listings/:id/edit", async (req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+app.use((err, req, res, next) => {
+    let {statusCode = 500, message="something went wrong"} = err;
+    res.status(statusCode).render("error.ejs", {message});
+    // res.status(statusCode).send(message);
 });
 
-// update route
-app.put("/listings/:id", async (req, res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-});
-// Delete Route
-app.delete("/listings/:id", async (req, res) => {
-    let {id}= req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-});
-// app.get("/testListing", async (req, res) => {
-//     let sampleListing = new Listing({
-//         title: "my fav food",
-//         description: "trending food was listed in the food menu",
-//         price: 200,
-//         location: "kolkata, kalyani",
-//         country: "india",
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("successful testing");
-// });
-
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+app.listen(3000, () => {
+    console.log("server is listening to port 3000");
 });
